61f7b4b613f4422b402043b58561104ea9730d50
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-25 13:40:30 +0300

    Initial Commit

---
+++ SlidingPanel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlidingPanel
+//
+//  Created by Ivelin Ivanov on 9/25/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlidingPanel/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlidingPanel
+//
+//  Created by Ivelin Ivanov on 9/25/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
